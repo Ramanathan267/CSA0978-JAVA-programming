461a7fb148e2ec6b672082980c9e21a7d8150b49
Ramanathan267 <124113980+Ramanathan267@users.noreply.github.com>
2024-07-13 13:33:26 +0530

    Add files via upload

---
+++ ak.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+public class ak {
+    public static void main(String[] args) {
+        
+    
+    Scanner input=new Scanner(System.in);
+    float n=input.nextFloat();
+    System.out.print("square: "+(n*n));
+    System.out.print("cube: "+(n*n*n));}}
